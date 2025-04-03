91cb0a41887f4f514697652db8d5bc28623ec335
wlals <jiminelp@gmail.com>
2025-04-03 10:30:43 +0900

    del :: 주석 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,14 +19,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      // type: 'mysql',
-      // host: 'localhost',
-      // port: 3306,
-      // username: 'root',
-      // password: 'wlals@0529',
-      // database: 'minlog_database',
-      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      // synchronize: true,
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
         host: 'localhost',
